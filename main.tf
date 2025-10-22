846bc11c7d6c5dbfa25e0c499b55274cc8889a2a
JiaFu89 <Jia_Fu1989@hotmail.com>
2025-10-22 21:07:13 +0800

    Fix TFLint

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
-    required_version = ">= 1.0.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
-    key    = "jiafu-s3-tf-ci.tfstate"  #Change this
+    key    = "jiafu-s3-tf-ci.tfstate" #Change this
     region = "us-east-1"
   }
 }
@@ -24,8 +24,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
